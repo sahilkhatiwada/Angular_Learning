88f6119ce17b393f785077a21909f8fdb3e5a5ca
Sahil Khatiwada <51027994+sahilkhatiwada@users.noreply.github.com>
2025-06-02 22:56:22 +0545

    Cookies

--- src/app/storage/storage.component.ts
+++ src/app/storage/storage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-storage',
@@ -8,7 +9,11 @@ import { Component } from '@angular/core';
 })
 export class StorageComponent {
 
+  constructor(private cookie : CookieService) {}
+
   sessionValue: string = "";
+  cookieValue: string = "";
+  localValue: string = "";
 
   setSessionStorage() {
     // sessionStorage.setItem('name', 'session');
@@ -17,7 +22,7 @@ export class StorageComponent {
 
   getSessionStorage() {
    this.sessionValue = sessionStorage.getItem('pass') ?? '';
-   
+
     
     
   }
@@ -29,5 +34,41 @@ export class StorageComponent {
   clearSessionStorage() {
    sessionStorage.clear();
    this.sessionValue = '';
+  }
+
+  // localstorage methods
+  setLocalStorage() {
+    localStorage.setItem('name', 'local');
+    localStorage.setItem('pass', 'local1234');
+  }
+  getLocalStorage() {
+    this.localValue = localStorage.getItem('pass') ?? '';
+  }
+  removeLocalStorage() {
+    localStorage.removeItem('name');
+    this.localValue = '';
+  }
+  clearLocalStorage() {
+    localStorage.clear();
+    this.localValue = '';
+  }
+
+
+
+  // cookie methods
+  setCookie() {
+    this.cookie.set('name', 'cookie',1);
+    this.cookie.set('pass', 'cookie1234');
+  }
+  getCookie() {
+    this.cookieValue = this.cookie.get('pass') ?? '';
+  }
+  removeCookie() {
+    this.cookie.delete('name');
+    this.cookieValue = '';
+  }
+  clearCookie() {
+    this.cookie.deleteAll();
+    this.cookieValue = '';
   }
 }
